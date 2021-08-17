5efa07d4998821e73775838064c9ee2792691c66
Dmitrii Trofimov <probcf@gmail.com>
2021-08-17 18:13:40 +0300

    add Service between DAO and Controller

---
+++ src/main/java/com/dmitriitrofimov/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.dmitriitrofimov.spring.mvc_hibernate_aop.service;
+
+import com.dmitriitrofimov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+	public List<Employee> getAllEmployees();
+}
